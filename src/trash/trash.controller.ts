9a6743cb734f92263e52c8c452db6894fd1d2f1f
Hans Tanawi <hans.tanawi@outlook.com>
2023-05-19 22:11:13 +0800

    wip: trash module

--- src/trash/trash.controller.ts
+++ src/trash/trash.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 
 @Controller('trash')
-export class TrashController {}
+export class TrashController {
+  @Get()
+  public getAllTrashes(@Param() type: TrashType) {
+    return [];
+  }
+
+  public getSingleTrash(@Query() trashId: string) {
+    return {};
+  }
+
+  @Post()
+  public addNewTrash(@Body() newTrash: any) {
+    return {};
+  }
+
+  @Put()
+  public updateTrash(@Query() trashId: string, @Body() trash: any) {
+    return {};
+  }
+}
